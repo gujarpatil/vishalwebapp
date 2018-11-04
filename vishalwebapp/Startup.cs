c8f6de8e301f23c208ba6e409d203d903a14f1e9
rajveergujarpatil2016 <rajveergujarpatil2016@outlook.com>
2018-11-04 12:13:22 +0000

    Comment vishal

---
+++ vishalwebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vishalwebapp.Startup))]
+namespace vishalwebapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
